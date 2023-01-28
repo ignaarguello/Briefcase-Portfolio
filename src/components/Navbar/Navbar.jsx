95e30eb4db93d06c9843f1490c36a66387baf0bc
ignaarguello <igna.arguello2110@hotmail.com>
2023-01-28 14:58:25 -0300

    Estilos a navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div id='container-general__navbar'>
+      <div className="item-nav__navbar">Home</div>
+      <div className="item-nav__navbar">About me</div>
+      <div className="item-nav__navbar">More+</div>
+    </div>
   )
 }
