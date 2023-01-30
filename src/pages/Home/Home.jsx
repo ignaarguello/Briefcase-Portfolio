5f41e481af06181296c715f55814f473efb55835
ignaarguello <igna.arguello2110@hotmail.com>
2023-01-30 17:03:28 -0300

    Titulos Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import PhotosGrid from '../../components/PhotosGrid/PhotosGrid'
 export default function Home() {
   return (
     <div id='container-home-page__home'>
+      <h2 id='titulo-ignacio__home'>Argüello Ignacio</h2>
+      <h4 id='titulo-fullstack__home'>Full Stack Developer - Mobile and Web</h4>
       <PhotosGrid/>
     </div>
   )
