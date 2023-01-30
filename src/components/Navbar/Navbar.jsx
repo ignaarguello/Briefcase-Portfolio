7ec86bc0be759eccf5601a18524433f1af1c2553
ignaarguello <igna.arguello2110@hotmail.com>
2023-01-30 16:47:43 -0300

    Photos Grid

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div id='container-general__navbar'>
       <img id='logo-portfolio__navbar' src="./logo-port2.png" alt="" />
       <div id='container-refs__navbar'>
-        <div className="item-nav__navbar">Home</div>
-        <div className="item-nav__navbar">About me</div>
-        <div className="item-nav__navbar">More+</div>
+        <Link to='/' className="item-nav__navbar">Home</Link>
+        <Link to='/about' className="item-nav__navbar">About me</Link>
+        <Link to='/more' className="item-nav__navbar">More+</Link>
       </div>
     </div>
   )
--- src/components/PhotosGrid/PhotosGrid.jsx
+++ src/components/PhotosGrid/PhotosGrid.jsx
@@ -3,6 +3,14 @@ import './PhotosGrid.css'
 
 export default function PhotosGrid() {
   return (
-    <div>PhotosGrid</div>
+    <div id='container-general__photosGrid'>
+      <div id='container-vertical__photosGrid'>
+        <img id='img-vertical__photosGrid' src="../foto2.png" alt="" />
+      </div>
+      <div id='container-horizontal__photosGrid'>
+        <img id='img-horizontal__photosGrid' src="../foto1.jpg" alt="" />
+        <img id='img-horizontal__photosGrid' src="../foto3.png" alt="" />
+      </div>
+    </div>
   )
 }
