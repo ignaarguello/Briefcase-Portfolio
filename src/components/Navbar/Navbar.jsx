36108a62a605253816bb63acbab3700414720cb4
ignaarguello <igna.arguello2110@hotmail.com>
2023-01-31 00:19:34 -0300

    Mejoras en fotos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <div id='container-general__navbar'>
-      <img id='logo-portfolio__navbar' src="./logo-port2.png" alt="" />
+      <img id='logo-portfolio__navbar' src="./images/logo-port2.png" alt="" />
       <div id='container-refs__navbar'>
         <Link to='/' className="item-nav__navbar">Home</Link>
         <Link to='/about' className="item-nav__navbar">About me</Link>
--- src/components/PhotosGrid/PhotosGrid.jsx
+++ src/components/PhotosGrid/PhotosGrid.jsx
@@ -5,12 +5,12 @@ import 'animate.css';
 export default function PhotosGrid() {
   return (
     <div id='container-general__photosGrid'>
-      <div id='container-vertical__photosGrid' className='animate__animated animate__fadeIn animate__delay-1s'>
-        <img id='img-vertical__photosGrid' src="../foto2.png" alt="" />
+      <div id='container-vertical__photosGrid' className='animate__animated animate__fadeIn animate__delay-2s'>
+        <img id='img-vertical__photosGrid' src="../images/foto3.png" alt="" />
       </div>
       <div id='container-horizontal__photosGrid' className='animate__animated animate__fadeIn animate__delay-2s'>
-        <img id='img-horizontal__photosGrid' src="../foto1.jpg" alt="" />
-        <img id='img-horizontal__photosGrid' src="../foto3.png" alt="" />
+        <img id='img-horizontal__photosGrid' src="../images/foto1.jpg" alt="" />
+        <img id='img-horizontal__photosGrid' src="../images/foto1-color.png" alt="" />
       </div>
     </div>
   )
