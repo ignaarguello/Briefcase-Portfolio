321ba9c459240fd76cb8dd10dd510f7438fe3358
ignaarguello <igna.arguello2110@hotmail.com>
2023-01-31 00:46:19 -0300

    Barra Lateral Izquierda

---
+++ src/components/BarraIzquierda/BarraIzquierda.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './BarraIzquierda.css'
+
+export default function BarraIzquierda() {
+  return (
+    <div id='barra-izquierda'></div>
+  )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import PhotosGrid from '../../components/PhotosGrid/PhotosGrid'
 import 'animate.css';
+import BarraIzquierda from '../../components/BarraIzquierda/BarraIzquierda';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <h2 id='titulo-ignacio__home'>_Argüello Ignacio</h2>
       <h4 id='titulo-fullstack__home'>Full Stack Developer MERN - Mobile and Web</h4>
       <PhotosGrid/>
+      <BarraIzquierda/>
     </div>
   )
 }
