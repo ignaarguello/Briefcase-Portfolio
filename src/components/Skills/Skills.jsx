e2654ffeade334aaad6126608b5c0ee5c6b81bc9
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-01 20:33:36 -0300

    estilos a skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -9,9 +9,19 @@ export default function Skills() {
     useEffect(() => {
         AOS.init();
       }, [])
+
+    const imagesSkills = [
+        'https://cdn-icons-png.flaticon.com/512/5968/5968267.png',
+    ]
+
+
+
     return (
         <div id='container-general__Skills'>
             <h2 id='titulo-stack__Skills' data-aos="zoom-in" data-aos-once="true" data-aos-duration="1000">- Stack de Tecnologías -</h2>
+            <div id='container-images__Skills'>
+
+            </div>
         </div>
     )
 }
