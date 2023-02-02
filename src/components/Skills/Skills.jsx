2cdce00edf34232f36b772b9018bd2ee2cede978
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-01 21:06:48 -0300

    Imagenes de skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -16,6 +16,12 @@ export default function Skills() {
         'https://cdn-icons-png.flaticon.com/512/1199/1199124.png',
     ]
 
+    const imagesReactNode = [
+        'https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_node_icon_130301.png',
+        'https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_wordmark_logo_icon_146375.png',
+        'https://cdn.icon-icons.com/icons2/112/PNG/512/python_18894.png',
+    ]
+
 
 
     return (
@@ -23,6 +29,11 @@ export default function Skills() {
             <h2 id='titulo-stack__Skills' data-aos="zoom-in" data-aos-once="true" data-aos-duration="1000">- Stack de Tecnologías -</h2>
             <div id='container-images__Skills'>
                 {imagesSkills.map((element) => 
+                    <img className='image-skill__skills' src={element} alt="" />
+                )}
+            </div>
+            <div id='container-images__Skills2'>
+                {imagesReactNode.map((element) => 
                     <img className='image-skill__skills' src={element} alt="" />
                 )}
             </div>
