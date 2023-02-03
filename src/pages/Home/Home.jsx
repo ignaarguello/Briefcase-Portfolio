6e9abf16e8022b6e2b4dd3c88d7ec167a1bcd4e9
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-03 19:52:23 -0300

    estilos generales

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div id='container-home-page__home' className='animate__animated animate__fadeIn animate__delay-1s'>
       <div id='container-grid-titulos__home'>
         <div id='container-titulos__home'>
-          <h2 id='titulo-ignacio__home'>_Argüello Ignacio</h2>
+          <h2 id='titulo-ignacio__home'>Argüello Ignacio</h2>
           <h4 id='titulo-fullstack__home'>Full Stack Developer MERN - Web and Mobile</h4>
           <button id='button-descargarCV__home'>Descargar CV</button>
         </div>
