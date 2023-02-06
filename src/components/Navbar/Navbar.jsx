3770f8d658b531c8ca33c510b9f32159969e359c
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-05 22:22:34 -0300

    Ultimo Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom';
 
+
 export default function Navbar() {
   return (
     <div id='container-general__navbar'>
