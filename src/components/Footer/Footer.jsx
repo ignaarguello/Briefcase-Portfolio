b046fa83138fd11d2d74e6092f99c25d8a569302
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-05 22:18:52 -0300

    Ultimo Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Footer.css'
 import { Link } from 'react-router-dom';
 
+
 export default function Footer() {
   return (
     <div id='container-general__footer'>
