3be427f9fc79905af6d72b9304bde0f3cec2d057
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-07 16:18:56 -0300

    Correciones en skill y footer

--- src/components/BarraIzquierda/BarraIzquierda.jsx
+++ src/components/BarraIzquierda/BarraIzquierda.jsx
@@ -3,6 +3,6 @@ import './BarraIzquierda.css'
 
 export default function BarraIzquierda() {
   return (
-    <div id='barra-izquierda'></div>
+    <div id='barra-izquierda' data-aos="zoom-out-up" data-aos-once="true" data-aos-duration="1000"></div>
   )
 }
