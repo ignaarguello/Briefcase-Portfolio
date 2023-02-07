b5667a9d8fa448b0f78ff0a102666b412cff8025
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-07 14:09:13 -0300

    Estilos al resposive

--- src/components/InfoMia/InfoMia.jsx
+++ src/components/InfoMia/InfoMia.jsx
@@ -6,16 +6,16 @@ export default function InfoMia() {
     return (
         <div id='container-general__infoMia'>
             <p className='texto-about__infoMia'>
+                Soy amante del desarrollo de Software, me fascina crear interfaces
+                y experiencias de usuario óptimas, modernas e innovadoras.
+            </p>
+            <p className='texto-about__infoMia'>
                 Apasionado por la tecnología y su impacto en la vida cotidiana
                 y en el mundo.
             </p>
             <p className='texto-about__infoMia'>
                 Perfil analista en cada aspecto de mi vida. Observador,
                 detallista, comprometido, pero sobre todo optimista.
-            </p>
-            <p className='texto-about__infoMia'>
-                Soy amante del desarrollo de Software, me fascina crear interfaces
-                y experiencias de usuario óptimas, modernas e innovadoras.
             </p>
             <div id='container-flecha__infoMia'>
                 <HiArrowDown id='flecha-abajo' className='animate__animated animate__bounce animate__slower animate__infinite' />
