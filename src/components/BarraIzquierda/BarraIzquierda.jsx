03699d292eba140f6e2ca9b1ebcd0de0760c5de6
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-13 22:35:17 -0300

    last commit

--- src/components/BarraIzquierda/BarraIzquierda.jsx
+++ src/components/BarraIzquierda/BarraIzquierda.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './BarraIzquierda.css'
 
+
 export default function BarraIzquierda() {
   return (
     <div id='barra-izquierda' data-aos="zoom-out-up" data-aos-once="true" data-aos-duration="1000"></div>
