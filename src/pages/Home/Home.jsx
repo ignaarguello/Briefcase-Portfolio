8849701fd39abd65a47698dcd0afe82b616e83f7
ignaarguello <igna.arguello2110@hotmail.com>
2023-02-15 16:15:49 -0300

    ultimo commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,12 @@ import 'animate.css';
 import BarraIzquierda from '../../components/BarraIzquierda/BarraIzquierda';
 import InfoMia from '../../components/InfoMia/InfoMia'
 import Skills from '../../components/Skills/Skills';
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+
+  
+
   return (
     <div id='container-home-page__home' className='animate__animated animate__fadeIn animate__delay-1s'>
       <div id='container-grid-titulos__home'>
